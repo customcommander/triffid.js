51cd5e2a65f9a40e7c4ed1cde99d8ff650c57451
customcommander <hello@spinjs.com>
2014-09-10 09:49:08 +0100

    add yuidoc for Assert

--- src/js/assert.js
+++ src/js/assert.js
@@ -1,5 +1,25 @@
 
+/**
+ * Standard Assertions
+ *
+ * @module Triffid
+ * @namespace Triffid
+ * @class Assert
+ * @static
+ */
 T.Assert = {
+
+    /**
+     * Asserts that a value is true (and not truthy).
+     *
+     * @example
+     *     Triffid.Assert.isTrue(true); // pass
+     *     Triffid.Assert.isTrue(1);    // fail
+     *
+     * @method isTrue
+     * @param value {Any} The value to test.
+     * @param [message] {String} Failure message should the assertion fails.
+     */
     isTrue: function (value, message) {
         if (value !== true) {
             throw new Error(message);
