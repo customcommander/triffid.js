9884411b54063988fe41f9186402d99f80bccef7
customcommander <hello@spinjs.com>
2014-09-13 11:04:12 +0000

    document methods in the Queue class

--- src/js/queue.js
+++ src/js/queue.js
@@ -1,4 +1,11 @@
 
+/**
+ * Jobs Queue Class
+ *
+ * @module Triffid
+ * @class Triffid.Queue
+ * @constructor
+ */
 function Queue() {
     this.jobs = [];
 }
@@ -7,15 +14,37 @@ Queue.prototype = {
 
     empty: false,
 
+    /**
+     * Add a new job to the queue.
+     *
+     * @method add
+     * @param name {String} Job name.
+     * @param fn {Function} The job itself.
+     * @param [context] {Object} The context in which the job executes.
+     * @public
+     * @chainable
+     */
     add: function (name, fn, context) {
         this.jobs.push( new Job(name, fn, context) );
         return this;
     },
 
+    /**
+     * Runs the next job in the queue.
+     *
+     * @method next
+     * @public
+     */
     next: function () {
         this.run();
     },
 
+    /**
+     * Runs the first job in the queue.
+     *
+     * @method run
+     * @public
+     */
     run: function () {
 
         var job = this.jobs.shift();
