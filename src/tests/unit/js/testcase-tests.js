e0ba911f8301d7d26c7b39ff59c2d4752bc9c584
customcommander <hello@spinjs.com>
2014-09-17 23:12:22 +0100

    add name to test cases

--- src/tests/unit/js/testcase-tests.js
+++ src/tests/unit/js/testcase-tests.js
@@ -1,17 +1,25 @@
-Triffid.suite('test cases')
+Triffid.suite('test case tests')
     .add({
+
+        name: 'smoke tests',
+
         'should run all the tests': function () {
 
             var testcase;
             var counter = 0;
 
             testcase = new Triffid.TestCase({
+
+                name: 'test1',
+
                 'should run test 1': function () {
                     counter++;
                 },
+
                 'should run test 2': function () {
                     counter++;
                 },
+
                 'should run test 3': function () {
                     counter++;
                 }
