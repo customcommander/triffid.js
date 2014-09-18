0836b9e7c8aae8f644620151d491d4ecc2d10f2e
customcommander <hello@spinjs.com>
2014-09-18 11:36:44 +0100

    add a test to make sure that tests are always run sequentially

--- src/tests/unit/js/testcase-tests.js
+++ src/tests/unit/js/testcase-tests.js
@@ -30,5 +30,34 @@ Triffid.suite('test case tests')
             this.waitFor(function () { return testcase.isFinished(); }, 50, function () {
                 Triffid.Assert.isTrue( 3 === counter, 'expected three tests to have been run');
             });
+        },
+
+        'should wait for a test to finish before running another': function () {
+
+            var testcase;
+            var flag = '';
+
+            testcase = new Triffid.TestCase({
+
+                name: 'test2',
+
+                'test 1': function () {
+                    this.wait(function () { flag += 'a'; }, 1000);
+                },
+
+                'test 2': function () {
+                    this.wait(function () { flag += 'b'; }, 500);
+                },
+
+                'test 3': function () {
+                    this.wait(function () { flag += 'c'; }, 250);
+                }
+            });
+
+            testcase.run();
+
+            this.waitFor(function () { return testcase.isFinished(); }, 50, function () {
+                Triffid.Assert.isTrue( 'abc' === flag, 'expected tests to have run sequentially' );
+            });
         }
     });
