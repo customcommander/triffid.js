1fc58230913173a54235eb6a3a54fe476358c6a0
customcommander <hello@spinjs.com>
2014-09-18 23:50:48 +0100

    add Assert.throwsError()

--- src/js/assert.js
+++ src/js/assert.js
@@ -56,5 +56,63 @@ T.Assert = {
         message+= "but got " + value + " (" + (typeof value) + ") instead.";
 
         T.fail(message);
+    },
+
+    /**
+     * Asserts that an error is thrown when given function is executed.
+     *
+     * @example
+     *     // Simply checks that a function throws an error: pass
+     *     Triffid.Assert.throwsError(function () {
+     *         throw new Error();
+     *     });
+     *
+     *     // Checks that the error is of a certain type: pass
+     *     Triffid.Assert.throwsError(function () {
+     *         throw new MyError();
+     *     }, MyError);
+     *
+     *     // Checks that the error message is the one we expect: pass
+     *     Triffid.Assert.throwsError(function () {
+     *         throw new Error('bad call');
+     *     }, 'bad call');
+     *
+     * @method throwsError
+     * @param func {Function} A function that should throw an error.
+     * @param [err] {Function|String} The error constructor or the error message
+     * @param [message] {String} Failure message should the assertion fails.
+     */
+    throwsError: function (func, err, message) {
+
+        var didnt_throw = false;
+
+        try {
+            func();
+            didnt_throw = true;
+        } catch (e) {
+
+            if (typeof err === "function" && !(e instanceof err)) {
+                message = message || "";
+                message+= "\n";
+                message+= "Error thrown is not from expected constructor.";
+
+                T.fail(message);
+
+            } else if (typeof err === "string" && e.message && e.message !== err) {
+                message = message || "";
+                message+= "\n";
+                message+= "Expected error message to have been '"+ e.message +"' ";
+                message+= "but got '"+err+"' instead.";
+
+                T.fail(message);
+            }
+        }
+
+        if (didnt_throw) {
+            message = message || "";
+            message+= "\n";
+            message+= "Expected an error to have been thrown but it didn't.";
+            T.fail(message);
+        }
     }
 };
--- src/tests/unit/js/assert-tests.js
+++ src/tests/unit/js/assert-tests.js
@@ -8,5 +8,40 @@ Triffid.suite('assert tests')
 
         'test: isFalse()': function () {
             Triffid.Assert.isFalse(false, 'expected false to be false');
+        },
+
+        'test: throwsError()': function () {
+
+            function MyError() {}
+
+            Triffid.Assert.throwsError(function () {
+                throw new Error('xxx');
+            }, null, 'should have succeeded without the second argument');
+
+            Triffid.Assert.throwsError(function () {
+                throw new MyError();
+            }, MyError, 'should have succeeded if the error is an instance of given constructor');
+
+            Triffid.Assert.throwsError(function () {
+                throw new Error('foobar');
+            }, 'foobar', 'should have succeeded if error message matches');
+
+            Triffid.Assert.throwsError(function () {
+                Triffid.Assert.throwsError(function () {
+                    // empty function intended
+                });
+            }, null, 'should have failed if the function does not throw an error');
+
+            Triffid.Assert.throwsError(function () {
+                Triffid.Assert.throwsError(function () {
+                    throw new MyError();
+                }, Error);
+            }, null,  'should have failed if error is not from expected constructor');
+
+            Triffid.Assert.throwsError(function () {
+                Triffid.Assert.throwsError(function () {
+                    throw new Error('abc');
+                }, 'xyz');
+            }, null,  'should have failed if error message does not match');
         }
     })
